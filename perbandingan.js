d7f55ca7a80ab635c41e27b30772f85f27694082
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-02 11:02:30 +0700

    inisiasi project

---
+++ perbandingan.js
@@ -0,0 +1,30 @@
+console.log('10 dari setiap operator perbandingan')
+let perbandingan1 = 20 < 10
+let perbandingan2 = 15 > 5
+let perbandingan3 = 70 >= 10
+let perbandingan4 = 10 <= 10
+let perbandingan5 = 40 == 40
+let perbandingan6 = 20 === 10
+let perbandingan7 = 15 != 10
+let perbandingan8 = 30 !== 15
+
+hasil1 = perbandingan1 
+console.log('perbandingan1', hasil1)
+hasil2 = perbandingan2 
+console.log('perbandingan2', hasil2)
+hasil3 = perbandingan3 
+console.log('perbandingan3', hasil3)
+hasil4 = perbandingan4
+console.log('perbandingan4', hasil4)
+hasil5 = perbandingan5 
+console.log('perbandingan5', hasil5)
+hasil6 = perbandingan6
+console.log('perbandingan6', hasil6)
+hasil7 = perbandingan7
+console.log('perbandingan7', hasil7)
+hasil8 = perbandingan8
+console.log('perbandingan8', hasil8)
+hasil9 = perbandingan6 > perbandingan4
+console.log('perbandingan6 > perbandingan4', hasil9)
+hasil10 = perbandingan2 < perbandingan8 == perbandingan7 != perbandingan5
+console.log('perbandingan2 < perbandingan8 == perbandingan7 != perbandingan5', hasil10)
---
+++ perhitungan.js
@@ -0,0 +1,31 @@
+console.log('operator perhintungan aritmatika')
+let angka1 = 20 + 20 + 20 + 20 + 20
+let angka2 = 10 + 10 - 10 * 10 / 10
+let angka3 = 30 * 30 + 30 - 30 ** 30
+let angka4 = 2 ** 4 - 7 + 10 * 15
+let angka5 = 4 % 10 + 10 - 16 / 5
+let angka6 = 9 * 20 - 10 + 15 / 2
+let angka7 = 20 - 15 + 25 * 8 % 2
+let angka8 = 9 * 10 + 20 - 30 / 5
+let angka9 = 15 + 20 - 10 * 20 - 20
+let angka10 = 50 % 2 + 15 - 10 * 5
+
+let angka11 = 9 * 10 + 20 - 30 / 5
+let angka12 = 15 + 20 - 10 * 20 - 20
+let angka13 = 50 % 2 + 15 - 10 * 5
+let angka14 = 4 % 10 + 10 - 16 / 5
+let angka15 = 9 * 20 - 10 + 15 / 2
+let angka16 = 20 - 15 + 25 * 8 % 2
+let angka17 = 10 + 10 - 10 * 10 / 10
+let angka18 = 30 * 30 + 30 - 30 ** 30
+let angka19  = 2 ** 4 - 7 + 10 * 15
+let angka20 = 4 % 10 + 10 - 16 / 5
+
+aritmatika = angka1 + angka2 - angka3 / angka4 * angka5 ** angka6 % angka7
+console.log('jumlah', aritmatika)
+aritmatika = angka7 + angka8 - angka9 / angka10 * angka11 ** angka12 % angka13
+console.log('jumlah', aritmatika)
+aritmatika = angka11 + angka12 - angka13 / angka14 * angka15 ** angka16 % angka17 
+console.log('jumlah', aritmatika)
+aritmatika = angka17 + angka18 - angka19 / angka20 * angka1 ** angka2 % angka3
+console.log('jumlah', aritmatika)
